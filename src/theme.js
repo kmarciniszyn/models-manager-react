f239ac5f604b3b529799c4b4de4e8edfdfd29126
Karolina Marciniszyn <marciniszyn.karolina@gmail.com>
2022-03-27 23:11:38 +0200

    updating theme

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,31 @@
 const colors = {
+    cyan: "#2BFFEA",
     lightBlue: "##A3D3FF",
-    mediumBlue: "#319CFF",
-    darkBlue: "##004F99",
+    mediumBlue: "#45C2FF",
+    darkBlue: "#0261C7",
+    claret: "#a30000",
     white: "#fff",
     black: "#252525",
     lightGray: "#f7f7f7",
+    mediumGray: "#c7c7c7",
     darkGray: "#777777",
+    transparentBlack: "rgba(0,0,0,.2)",
 };
 
 const common = {
     breakpoints: {
         maxMobileWidth: 767,
     },
+    boxShadow: {
+        section: `0px 8px 18px -10px ${colors.transparentBlack}`,
+        button: {
+            hover: `0 5px 10px ${colors.transparentBlack}`,
+            active: `0 2px 5px ${colors.transparentBlack}`,
+        },
+        input: {
+            focus: `0 2px 5px ${colors.transparentBlack}`,
+        },
+    }
 };
 
 export const theme = {
@@ -21,6 +35,34 @@ export const theme = {
         textPrimary: colors.black,
         site: {
             background: colors.lightGray,
+        },
+        section: {
+            background: colors.white,
+            borderBottom: colors.mediumGray,
+        },
+        listItem: {
+            borderBottom: colors.mediumGray,
+        },
+        button: {
+            background: colors.lightGray,
+            text: colors.darkBlue,
+            textSpecial: colors.claret,
+        },
+        submit: {   
+            gradient: `linear-gradient(to right,
+                ${colors.darkBlue},
+                ${colors.cyan})`,
+            text: colors.lightGray,
+        },
+        input: {
+            borderBottom: colors.mediumGray,
+            placeholder: colors.darkGray,
+            invalid: colors.claret,
+        },
+        icon: {
+            login: {
+                text: colors.mediumGray,
+            },
         },
     }
 };
