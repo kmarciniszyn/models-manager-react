d080a590691afb62d383032b59eeed582040cd9c
Karolina Marciniszyn <marciniszyn.karolina@gmail.com>
2022-03-30 19:31:53 +0200

    change code to demonstrate how app works

--- src/features/models/ModelsPage/getModelsNamesArray.js
+++ src/features/models/ModelsPage/getModelsNamesArray.js
@@ -2,7 +2,23 @@ import axios from "axios";
 import { serverURL, searchModelsPath } from "../../../paths";
 
 const getModelsArray = async () =>
-    axios.post(`${serverURL()}${searchModelsPath}`)
-        .then(response => response.data)
+// the code commented below is for getting a models array (with models names) using a connection to the api
+
+/* axios.post(`${serverURL()}${searchModelsPath}`)
+    .then(response => response.json())*/
+
+
+// the code below is only added to demonstrate how the app works; models.json file is in public file
+{
+    const response = await fetch("/models.json");
+
+    if (!response) {
+        new Error(response.statusText);
+    }
+
+    const jsonObject = await response.json();
+
+    return jsonObject.models_list;
+};
 
 export default getModelsArray;
--- src/features/models/ModelsPage/getModelsWithState.js
+++ src/features/models/ModelsPage/getModelsWithState.js
@@ -3,10 +3,27 @@ import { serverURL, getModelStatePath } from "../../../paths";
 import getModelsNamesArray from "./getModelsNamesArray";
 
 const getModelState = async (modelFullName) =>
-    axios.get(`${serverURL()}${getModelStatePath()}`, {
-        nazwa_modelu: modelFullName
-    })
-        .then(response => response.data)
+// the code commented below is for getting a model state using a connection to the api
+/*axios.get(`${serverURL()}${getModelStatePath()}`, {
+    nazwa_modelu: modelFullName
+})
+    .then(response => response)*/
+
+// the code below is only added to demonstrate how the app works; models.json file is in public file
+{
+    const response = await fetch("/models.json");
+
+    if (!response) {
+        new Error(response.statusText);
+    }
+
+    const jsonObject = await response.json();
+    const object = jsonObject.state;
+    let index = await object.findIndex(item => item.name === modelFullName);
+    let result = object[index];
+
+    return result.state;
+}
 
 const getModelsWithState = async () => {
     const modelsNamesArray = await getModelsNamesArray();
