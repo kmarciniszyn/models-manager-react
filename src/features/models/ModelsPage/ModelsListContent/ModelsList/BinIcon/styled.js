431a9f4fbccf2fe852bea94084e3dd5b3a75fa92
Karolina Marciniszyn <marciniszyn.karolina@gmail.com>
2022-03-31 12:22:06 +0200

    fixed height BinIcon

--- src/features/models/ModelsPage/ModelsListContent/ModelsList/BinIcon/styled.js
+++ src/features/models/ModelsPage/ModelsListContent/ModelsList/BinIcon/styled.js
@@ -3,7 +3,7 @@ import { ReactComponent as Icon } from "./bin.svg";
 
 export const BinIcon = styled(Icon)`
     color: ${({ theme }) => theme.colors.textPrimary};
-    height: 15px;
+    height: 20px;
 
     @media(max-width: ${({ theme }) => theme.breakpoints.maxMobileWidth}px){
         height: 15px;
